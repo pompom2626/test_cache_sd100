35d73fcb8952aac322d011075ae7a9f999f34d3c
pompom2626 <pompom2626@gmail.com>
2020-01-08 22:31:30 -0600

    the fifth : to fix activate service worker problem

--- sw_firstworker.js
+++ sw_firstworker.js
@@ -1,36 +1,54 @@
+const cacheVersion = "v1";
 var CACHE_NAME = 'first-sw';
 var urlsToCache = [
     '/',
 
 ];
 
-self.addEventListener('install', function(event) {
+//install
+self.addEventListener('install', function (event) {
     event.waitUntil(
         caches.open(CACHE_NAME)
-            .then(function(cache) {
+            .then(function (cache) {
                 console.log('Opened cache');
                 return cache.addAll(urlsToCache);
             })
     );
 });
-t
+
+//Activate Event
+self.addEventListener("activate", e => {
+    e.waitUntil(
+        caches.keys().then(cacheVersions => {
+            return Promise.all(
+                cacheVersions.map(cacheName => {
+                    if (cacheName !== cacheVersion) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+//fetch
 self.addEventListener('fetch', function (event) {
     event.respondWith(
         caches.match(event.request).then(function (response) {
-           
+
             if (response) {
                 return response;
             }
-          
-            var request = event.request.clone(); 
 
-            if (request.mode !== 'navigate' && request.url.indexOf(request.referrer) === -1) 						{
+            var request = event.request.clone();
+
+            if (request.mode !== 'navigate' && request.url.indexOf(request.referrer) === -1) {
                 request = new Request(request, { mode: 'no-cors' })
             }
 
             return fetch(request).then(function (httpRes) {
 
-                if (!httpRes  || ( httpRes.status !== 200 && httpRes.status !== 304 && httpRes.type !== 'opaque') || request.method === 'POST') {
+                if (!httpRes || (httpRes.status !== 200 && httpRes.status !== 304 && httpRes.type !== 'opaque') || request.method === 'POST') {
                     return httpRes;
                 }
 
